386ed64e9cd7869c302be069c4d3a144889f37cf
Ace Christian F. Serraon <aceserraon@gmail.com>
2022-02-14 13:42:39 +0900

    Update eos-lattice-constant-bulk-compute.py

--- examples/eos-lattice-constant-bulk-compute.py
+++ examples/eos-lattice-constant-bulk-compute.py
@@ -2,14 +2,19 @@ import numpy as np
 
 from ase import Atoms
 from ase.io.trajectory import Trajectory
-from ase.calculators.emt import EMT
+from state_interface.state import STATE
 
 a = 4.0  # approximate lattice constant
 b = a / 2
+
+input_data = {}
+
+dft_calc = STATE(label=label,input_data=input_data_
+
 ag = Atoms('Ag',
            cell=[(0, b, b), (b, 0, b), (b, b, 0)],
            pbc=1,
-           calculator=EMT())  # use EMT potential
+           calculator=dft_calc)  # use EMT potential
 cell = ag.get_cell()
 traj = Trajectory('Ag.traj', 'w')
 for x in np.linspace(0.95, 1.05, 5):
