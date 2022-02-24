9327f67f729f5e783cdbd880f9e9b81114fe112f
ueda <ueda@cp.prec.eng.osaka-u.ac.jp>
2022-02-24 12:17:53 +0900

    first pytest example

---
+++ tests/io/test_read_state_input.py
@@ -0,0 +1,14 @@
+import os
+from state_interface.io import read_state_input
+
+base = os.path.dirname(os.path.abspath(__file__))
+TEST_DIR = os.path.normpath(os.path.join(base, "../../data/io_test"))
+
+
+def test_read_al_input():
+    """test Al input file"""
+    data_path = os.path.join(TEST_DIR, "test_al/nfinp_scf")
+    atoms = read_state_input(data_path)
+    assert len(atoms.cell) == 3
+    assert atoms.symbols == ["Al"]
+    assert len(atoms.positions) == 1
