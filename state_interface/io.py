c97932654e88143cd3cbcabf400f9eb63779f026
Ryo Ueda <56352572+uedar@users.noreply.github.com>
2022-11-03 00:23:11 +0900

    Fix bug output parser (#34)
    * fix bug in parser code
    
    * Update io.py
    
    change extract == False to extract is False
    
    * satisfy pylint requirements
    
    * save the structure found in output file to calculator results
    
    * add method to update position
    
    * update
    
    * enable constraint
    
    * update state.py
    
    * satisfy lint requirements
    
    * change poetry installer
    
    * reduce the number of char
    
    * remove -
    
    * change command
    
    * change python version
    
    * pylint
    
    * replace dict
    
    Co-authored-by: Harry Halim <harry@cp.prec.eng.osaka-u.ac.jp>
    Co-authored-by: hhlim12 <77390746+hhlim12@users.noreply.github.com>

--- state_interface/io.py
+++ state_interface/io.py
@@ -1,10 +1,14 @@
+"""
+Read strucutures from STATE input files
+and read results from STATE output files.
+"""
 import re
 import warnings
 import numpy as np
 from ase import Atoms
 from ase.units import Hartree, Bohr
 
-force_unit = Hartree/Bohr
+force_unit = Hartree / Bohr
 
 
 def read_state_input(input_file):
@@ -24,69 +28,79 @@ def read_state_input(input_file):
     kwargs = {}
 
     # Load atomic coordinate (cartesian/crystal) to kwargs
-    pos_opt = info.blocks['ATOMIC_COORDINATES']['opt']
-    if pos_opt in [None, 'CART', 'CARTESIAN']:
+    pos_opt = info.blocks["ATOMIC_COORDINATES"]["opt"]
+    if pos_opt in [None, "CART", "CARTESIAN"]:
         # Converted position
-        pos = Bohr * info.blocks['ATOMIC_COORDINATES']['input']['cps']
-        kwargs['positions'] = pos
-    elif pos_opt in ['CRYSTAL', 'CRYS']:
-        pos = info.blocks['ATOMIC_COORDINATES']['input']['cps']
-        kwargs['scaled_positions'] = pos
+        pos = Bohr * info.blocks["ATOMIC_COORDINATES"]["input"]["cps"]
+        kwargs["positions"] = pos
+    elif pos_opt in ["CRYSTAL", "CRYS"]:
+        pos = info.blocks["ATOMIC_COORDINATES"]["input"]["cps"]
+        kwargs["scaled_positions"] = pos
     else:
         raise ValueError(f"Invalid atomic coordinate option = {pos_opt}.")
 
     # Load cell information
-    if 'CELL' in list(info.variables.keys()):
-        clist = [float(cnum) for cnum in info.variables['CELL']]
+    if "CELL" in list(info.variables):
+        clist = [float(cnum) for cnum in info.variables["CELL"]]
         # Converted lattice lengths
-        lena, lenb, lenc = Bohr*clist[0], Bohr*clist[1], Bohr*clist[2]
-        kwargs['cell'] = [lena, lenb, lenc, clist[3], clist[4], clist[5]]
-    elif 'CELL' in list(info.blocks.keys()):
-        cell_matrix = info.blocks['CELL']['input']
+        lena, lenb, lenc = Bohr * clist[0], Bohr * clist[1], Bohr * clist[2]
+        kwargs["cell"] = [lena, lenb, lenc, clist[3], clist[4], clist[5]]
+    elif "CELL" in list(info.blocks):
+        cell_matrix = info.blocks["CELL"]["input"]
         # Converted cell matrix
-        kwargs['cell'] = Bohr * cell_matrix
+        kwargs["cell"] = Bohr * cell_matrix
     else:
-        raise ValueError('Cell information not found.')
+        raise ValueError("Cell information not found.")
 
     # Load Atomic species
-    species = info.blocks['ATOMIC_SPECIES']['input']['symbol']
-    ityp = info.blocks['ATOMIC_COORDINATES']['input']['ityp']
-    symlist = [species[int(i)-1] for i in ityp]
-    kwargs['symbols'] = symlist
+    species = info.blocks["ATOMIC_SPECIES"]["input"]["symbol"]
+    ityp = info.blocks["ATOMIC_COORDINATES"]["input"]["ityp"]
+    symlist = [species[int(i) - 1] for i in ityp]
+    kwargs["symbols"] = symlist
 
     # Initiate ASE Atoms module
     structure = Atoms(**kwargs)
     return structure
 
+
 def read_state_output(output_file):
+    """ASE module to read STATE output files."""
+
     data = []
     extract = False
-    with open(output_file) as fd:
-        lines = fd.readlines()
-        if 'The calculation has converged' not in str(lines):
-            warnings.warn('The calculation has not converged')
+    with open(output_file, encoding="utf-8") as file:
+        lines = file.readlines()
+        if "The calculation has converged" not in str(lines):
+            warnings.warn("The calculation has not converged")
         for line in lines:
-            if re.search('CONVERGED', line):
+            if re.search("CONVERGED", line):
                 extract = True
+                count_empty_line = 0
             if extract:
                 data.append(line.split())
-                if re.search('EXIT', line):
+                if len(line.split()) == 0:
+                    count_empty_line += 1
+                if count_empty_line == 2:
                     extract = False
-    energy, f_max, f_rms = Hartree * \
-        float(data[2][1]), force_unit * \
-        float(data[2][2]), force_unit*float(data[2][3])
-    force_data = []
-    positions = []
-    species = []
-    for line in data[6:]:
-        if line == []:
-            break
-        species.append(line[2])
-        positions.append(line[3:6])
-        force_data.append(line[6:9])
-    positions = Bohr*np.array(positions, dtype=float)
-    structure = Atoms(symbols=species, positions=positions)
-    forces = force_unit*np.array(force_data, dtype=float)
+            if extract is False and len(data) != 0:
+                energy = Hartree * float(data[2][1])
+                #                f_max      = force_unit * float(data[2][2])
+                #                f_rms      = force_unit * float(data[2][3])
+                force_data = []
+                positions = []
+                species = []
+                for vip_line in data[6:]:
+                    if vip_line == []:
+                        break
+                    if vip_line[0] != "MD:":
+                        break
+                    species.append(vip_line[2])
+                    positions.append(vip_line[3:6])
+                    force_data.append(vip_line[6:9])
+                positions = Bohr * np.array(positions, dtype=float)
+                structure = Atoms(symbols=species, positions=positions)
+                forces = force_unit * np.array(force_data, dtype=float)
+                data = []
     return structure, energy, forces
 
 
@@ -120,20 +134,20 @@ class InputParser:
 
     def varblock(self):
         """Read and clean input file, and separate variable and block lines."""
-        with open(self.file_object, 'r') as f:
-            lines = f.read().splitlines()
+        with open(self.file_object, "r", encoding="utf-8") as file:
+            lines = file.read().splitlines()
 
         # Clean comments and trailing spaces
         clean = []
         for line in lines:
             _line = line.strip()
-            if not _line.startswith('#'):
+            if not _line.startswith("#"):
                 clean.append(line.strip())
         lines = clean.copy()
 
         # Separate variables and blocks
         for idx, line in enumerate(lines):
-            if line.startswith('&'):
+            if line.startswith("&"):
                 blkstart_idx = idx
                 break
         self.varlines = lines[:blkstart_idx]
@@ -153,22 +167,22 @@ class InputParser:
         for line in self.blklines:
             linestr = line.upper()
 
-            if linestr.startswith('&') and not linestr.startswith('&END'):
+            if linestr.startswith("&") and not linestr.startswith("&END"):
                 # Process first line of a block for name
                 # and option(if available)
                 parts = line.split()
-                blkname = parts[0].lstrip('&')
+                blkname = parts[0].lstrip("&")
                 blkinp, blkopt = [], None
                 # For Blocks with additional options
                 # (ex. &COORDINATE + CRYSTAL)
                 if len(parts) == 2:
                     blkopt = parts[1]
 
-            elif linestr.startswith('&END'):
+            elif linestr.startswith("&END"):
                 # Finalize block dictionary entry
                 if blkopt:
                     blkopt = blkopt.upper()
-                self.blocks[blkname] = {'opt': blkopt, 'input': blkinp}
+                self.blocks[blkname] = {"opt": blkopt, "input": blkinp}
 
             else:
                 blkinp.append(line)
@@ -180,27 +194,27 @@ class InputParser:
             if key.upper() in self.subroutine:
                 self.subroutine[key.upper()]()
 
-
     def manager(self):
         """Define the subroutine that further processes the block dictionary."""
+
         def atomic_coordinates_read():
-            entry = self.blocks['ATOMIC_COORDINATES']
-            lines = entry['input'].copy()
-            entry['input'] = dict()
+            entry = self.blocks["ATOMIC_COORDINATES"]
+            lines = entry["input"].copy()
+            entry["input"] = {}
             # The following are the EXACT syntax naming in
             _cps = [line.split()[:3] for line in lines]
-            entry['input']['cps'] = np.array(_cps, dtype=float)
+            entry["input"]["cps"] = np.array(_cps, dtype=float)
             _iwei = [line.split()[3] for line in lines]
-            entry['input']['iwei'] = _iwei
+            entry["input"]["iwei"] = _iwei
             _imdtyp = [line.split()[4] for line in lines]
-            entry['input']['imdtyp'] = _imdtyp
+            entry["input"]["imdtyp"] = _imdtyp
             _ityp = [line.split()[5] for line in lines]
-            entry['input']['ityp'] = _ityp
+            entry["input"]["ityp"] = _ityp
 
         def atomic_species_read():
-            entry = self.blocks['ATOMIC_SPECIES']
-            lines = entry['input'].copy()
-            entry['input'] = dict()
+            entry = self.blocks["ATOMIC_SPECIES"]
+            lines = entry["input"].copy()
+            entry["input"] = {}
             _symbol, _mass, _file = [], [], []
             for line in lines:
                 # For the moment atomic number is not supported
@@ -208,27 +222,27 @@ class InputParser:
                 _mass.append(line.split()[1])
                 _file.append(line.split()[2])
 
-            entry['input']['symbol'] = _symbol
-            entry['input']['mass'] = _mass
-            entry['input']['file'] = _file
+            entry["input"]["symbol"] = _symbol
+            entry["input"]["mass"] = _mass
+            entry["input"]["file"] = _file
 
         def cell_read():
-            entry = self.blocks['CELL']
-            _vector = [line.split() for line in entry['input']]
-            entry['input'] = np.array(_vector, dtype=float)
+            entry = self.blocks["CELL"]
+            _vector = [line.split() for line in entry["input"]]
+            entry["input"] = np.array(_vector, dtype=float)
 
         self.subroutine = {
-            'ATOMIC_COORDINATES': atomic_coordinates_read,
-            'ATOMIC_SPECIES': atomic_species_read,
-            'CELL': cell_read,
+            "ATOMIC_COORDINATES": atomic_coordinates_read,
+            "ATOMIC_SPECIES": atomic_species_read,
+            "CELL": cell_read,
         }
 
     def errorcheck(self):
         """Error definition and check of code breaking circumstances."""
         # CELL Block & CELL Variable incomptibility
-        if 'CELL' in self.variables.keys() and 'CELL' in self.blocks.keys():
+        if "CELL" in self.variables and "CELL" in self.blocks:
             raise ValueError("Double CELL declaration in block and variable.")
 
         # ATOMIC_COORDINATES not declared
-        if 'ATOMIC_COORDINATES' not in list(self.blocks.keys()):
+        if "ATOMIC_COORDINATES" not in list(self.blocks):
             raise ValueError("ATOMIC_COORDINATES is not declared.")
--- state_interface/state.py
+++ state_interface/state.py
@@ -2,102 +2,125 @@
 export ASE_STATE_COMMAND="/path/to/STATE < PREFIX.in > PREFIX.out"
 Run STATE jobs.
 """
-import warnings
-from ase import io
+
 from ase.units import Bohr, Hartree
-from ase.calculators.calculator import FileIOCalculator, PropertyNotPresent
-from ase.atoms import Atoms
-from ase.calculators.singlepoint import (SinglePointDFTCalculator)
-import re
-import numpy as np
+from ase.calculators.calculator import FileIOCalculator
+from ase.calculators.singlepoint import SinglePointDFTCalculator
 from ase.data import chemical_symbols
+import numpy as np
 from .io import read_state_output
 
-error_template = 'Property "%s" not available. Please try running STATE\n' \
-                 'first by calling Atoms.get_potential_energy().'
-warn_template = 'Property "%s" is None. Typically, this is because the ' \
-                'required information has not been printed by STATE ' \
-                'at a "low" verbosity level (the default). ' \
-                'Please try running STATE with "high" verbosity.'
+ERROR_TEMPLATE = (
+    'Property "%s" not available. Please try running STATE\n'
+    "first by calling Atoms.get_potential_energy()."
+)
+
+WARN_TEMPLATE = (
+    'Property "%s" is None. Typically, this is because the '
+    "required information has not been printed by STATE "
+    'at a "low" verbosity level (the default). '
+    'Please try running STATE with "high" verbosity.'
+)
 
 force_unit = Hartree / Bohr
 
+
 class STATE(FileIOCalculator):
     """
+    STATE calculator interface class
     """
-    implemented_properties = ['energy', 'forces']
-    command = 'STATE < PREFIX.in > PREFIX.out'
+
+    implemented_properties = ["energy", "forces"]
+    command = "STATE < PREFIX.in > PREFIX.out"
     discard_results_on_any_change = True
-    def __init__(self, label='state', atoms=None, **kwargs):
-        FileIOCalculator.__init__(self,
-                                  label, atoms, **kwargs)
+
+    def __init__(self, label="state", atoms=None, **kwargs):
+        FileIOCalculator.__init__(self, label, atoms, **kwargs)
         self.calc = None
-        self.label= label
-    
+        self.label = label
+
     def write_input(self, atoms, properties=None, system_changes=None):
         FileIOCalculator.write_input(self, atoms, properties, system_changes)
-        natom     = atoms.get_global_number_of_atoms()
-        coords    = atoms.get_positions() * 1 / Bohr
-        species   = list(set(atoms.get_chemical_symbols()))
+        natom = atoms.get_global_number_of_atoms()
+        coords = atoms.get_positions() * 1 / Bohr
+        species = list(set(atoms.get_chemical_symbols()))
         atomic_nums = atoms.get_atomic_numbers()
-        ntyp    = len(species)
-        cell     = atoms.get_cell() * 1 / Bohr
-        with open (self.label + '.in', 'w') as fd:
-            input_data = self.parameters['input_data']
-            print ("#\n#\n#", file = fd)
-            print("NTYP",   ntyp, file = fd)
-            print("NATM",   natom, file = fd)
+        ntyp = len(species)
+        cell = atoms.get_cell() * 1 / Bohr
+        constraints = atoms.constraints
+        with open(self.label + ".in", "w", encoding="utf-8") as file:
+            input_data = self.parameters["input_data"]
+            print("#\n#\n#", file=file)
+            print("NTYP", ntyp, file=file)
+            print("NATM", natom, file=file)
             for parameter in input_data.keys():
-                write_parameter(parameter, input_json=input_data,file=fd)
-            print ("&CELL", file = fd)
+                write_parameter(parameter, input_json=input_data, file=file)
+            print("&CELL", file=file)
             for cell_vector in cell:
-                print (*cell_vector, file = fd)
-            print ("&END", file = fd)
-            print("&ATOMIC_SPECIES", file = fd)
-            for line in input_data['PSEUDOS']:
-                print(*line, file = fd)
-            print("&END", file = fd)
-            print("&ATOMIC_COORDINATES", "CARTESIAN", file = fd)
-            pseudo_idx = {lst[0]: i+1 for i, lst in enumerate(input_data['PSEUDOS'])}
-            substrate_idx = input_data['SUBSTRATE_IDX'] if 'SUBSTRATE_IDX' in input_data else []
-            substrate_set = get_substrate_set(substrate_idx)
-            for i,line in enumerate(coords):
-                cs = chemical_symbols[atomic_nums[i]]
-                imdtyp = 0 if i+1 in substrate_set else 1
-                print(*line, 1, imdtyp ,pseudo_idx[cs], file=fd)
-            print ("&END", file = fd)
-            if 'VDW-DF' in input_data:
-                print('&VDW-DF', file = fd)
-                print("QCUT",  input_data['VDW-DF']['QCUT'], file = fd)
-                print("NQ",  input_data['VDW-DF']['NQ'], file = fd)
-                print('&END', file = fd)
-            
+                print(
+                    f"{cell_vector[0]: >18.12f}",
+                    f"{cell_vector[1]: >18.12f}",
+                    f"{cell_vector[2]: >18.12f}",
+                    file=file,
+                )
+            print("&END", file=file)
+            print("&ATOMIC_SPECIES", file=file)
+            for line in input_data["PSEUDOS"]:
+                print(*line, file=file)
+            print("&END", file=file)
+            print("&ATOMIC_COORDINATES", "CARTESIAN", file=file)
+            pseudo_idx = {
+                lst[0]: i + 1 for i, lst in enumerate(input_data["PSEUDOS"])
+            }
+
+            constraints_idx = (
+                constraints[0].get_indices() if len(constraints) > 0 else []
+            )
+            for i, line in enumerate(coords):
+                chemical_symbol = chemical_symbols[atomic_nums[i]]
+                imdtyp = 0 if i in constraints_idx else 1
+                print(
+                    f"{line[0]: >18.12f}",
+                    f"{line[1]: >18.12f}",
+                    f"{line[2]: >18.12f}",
+                    1,
+                    imdtyp,
+                    pseudo_idx[chemical_symbol],
+                    file=file,
+                )
+            print("&END", file=file)
+            if "VDW-DF" in input_data:
+                print("&VDW-DF", file=file)
+                print("QCUT", input_data["VDW-DF"]["QCUT"], file=file)
+                print("NQ", input_data["VDW-DF"]["NQ"], file=file)
+                print("&END", file=file)
+
     def read_results(self):
-        structure, energy, forces = read_state_output(self.label + '.out')
-        calc = SinglePointDFTCalculator(structure, energy=energy,
-                                        forces=forces)
-        
+        structure, energy, forces = read_state_output(self.label + ".out")
+        calc = SinglePointDFTCalculator(
+            structure, energy=energy, forces=forces
+        )
+
         self.calc = calc
         self.results = calc.results
+        self.results["updt_pos"] = structure.get_positions()
 
     def get_stress(self, atoms):
-        return np.zeros((3,3))
+        """Dummy class method to get stress"""
+        return np.zeros((3, 3))
 
-def write_parameter(parameter,input_json, file):
-    if parameter == 'KPOINT_MESH':
-        print('KPOINT_MESH', *input_json['KPOINT_MESH'], file=file)
-    elif parameter == 'KPOINT_SHIFT':
-        print('KPOINT_SHIFT', *input_json['KPOINT_SHIFT'], file=file)
-    elif parameter in ['PSEUDOS', 'SUBSTRATE_IDX', 'VDW-DF']:
+    def get_updt_pos(self):
+        """Return final atoms positions"""
+        return self.results["updt_pos"]
+
+
+def write_parameter(parameter, input_json, file):
+    """Helper method to write input parameters"""
+    if parameter == "KPOINT_MESH":
+        print("KPOINT_MESH", *input_json["KPOINT_MESH"], file=file)
+    elif parameter == "KPOINT_SHIFT":
+        print("KPOINT_SHIFT", *input_json["KPOINT_SHIFT"], file=file)
+    elif parameter in ["PSEUDOS", "SUBSTRATE_IDX", "VDW-DF"]:
         pass
     else:
         print(parameter, input_json[parameter], file=file)
-
-def get_substrate_set(substrate_ids):
-    id_set = set()
-    for ids in substrate_ids: 
-        start = re.search('^[0-9]+(?=:)', ids).group()
-        end = re.search('(?<=:)[0-9]+', ids).group()
-        lst = [i for i in range(int(start), int(end)+1)]
-        id_set |= set(lst)
-    return id_set
